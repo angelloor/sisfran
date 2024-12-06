142a140dfd0f902fff2c9c4c6dda1ee4b548ec57
Angel Loor <angelloorec@gmail.com>
2024-12-06 12:23:37 -0500

    Bugs in main

--- src/components/main/main.model.php
+++ src/components/main/main.model.php
@@ -23,7 +23,7 @@ class Main
     public function listarFuncionarioPorCategoriaMain($idCategoria)
     {
         $connection = new MySQLPDO();
-        $stmt = $connection->prepare("select distinct er.id_entrega_recepcion, p.id_persona, p.nombre_persona from entrega_recepcion er inner join activo a on er.activo_id = a.id_activo inner join categoria ca on a.categoria_id = ca.id_categoria inner join persona p on er.persona_id = p.id_persona where ca.id_categoria = :idCategoria order by p.nombre_persona asc;");
+        $stmt = $connection->prepare("select distinct p.id_persona, p.nombre_persona from entrega_recepcion er inner join activo a on er.activo_id = a.id_activo inner join categoria ca on a.categoria_id = ca.id_categoria inner join persona p on er.persona_id = p.id_persona where ca.id_categoria = :idCategoria order by p.nombre_persona asc;");
         $stmt->bindValue(":idCategoria", $idCategoria, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
